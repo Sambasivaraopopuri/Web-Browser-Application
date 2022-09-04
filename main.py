ea38f848b644fa67b53e854d9721a887f5568e1b
Sambasivaraopopuri <105603060+Sambasivaraopopuri@users.noreply.github.com>
2022-09-04 08:54:44 +0530

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import back, forward
 from PyQt5.QtWidgets import *
 import sys
 from PyQt5.QtWebEngineWidgets import *
@@ -9,8 +10,15 @@ class MainWindow(QMainWindow):
         self.browser.setUrl(QUrl("http://google.com"))
         self.setCentralWidget(self.browser)
         self.showMaximized()
+        nav_bar=QToolBar()
+        self.addToolBar(nav_bar)
+        back_button=QAction("Back",self)
+        back_button.triggered.connect(self.browser.back)
+        nav_bar.addAction(back_button)
 
-
+        forward_btn=QAction("Forward",self)
+        forward_btn.triggered.connect(self.browser.forward)
+        nav_bar.addAction(forward_btn)
 app=QApplication(sys.argv)
 QApplication.setApplicationName("SSR")
 window=MainWindow()
